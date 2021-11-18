3de8bbc7be9f9102d26fecd0a1caeea75c76d89e
LeeDowon <leedowon19@gmail.com>
2021-11-18 15:57:49 +0900

    Update webservice

---
+++ webservice/server/Api.js
@@ -0,0 +1,91 @@
+const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
+const app = express();
+const { Client } = require("pg");
+const Query = require('pg').Query
+const cors = require('cors');
+//const Auth = require('./Auth')
+
+const PORT = 3100;
+const HOST = '0.0.0.0';
+app.use(cors());
+
+
+//connect with db
+var client = new Client({ 
+    user : 'mywork',
+    host : 'eks-work-db.ck9mt4aiy0zp.ap-northeast-2.rds.amazonaws.com',
+    database : 'myworkdb',
+    password : "wIi~e1xIrj!C~Hv.",
+    port : 5432, })
+
+client.connect(err => { 
+    if (err) { console.error('connection error', err.stack) }
+    else { console.log('success!') }
+});
+
+function getToday(){
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = ("0" + (1 + date.getMonth())).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+
+    return year + "-" + month + "-" + day;
+}
+
+
+app.get('/api/today/:currencyCode', function(req, res, next) {
+    today = '2021-11-08'
+    console.log(today)
+    console.log(req.params.currencyCode)
+    console.log("SELECT * FROM " +req.params.currencyCode+  " WHERE date = '"+today+"'")
+    
+
+    const query = new Query("SELECT * FROM " +req.params.currencyCode+  " WHERE date = '"+today+"'");
+    client.query(query)
+        
+    var rows = [];
+     rows.push({updateDate : today })
+     query.on("row",row=>{
+          rows.push(row);
+     });
+     
+     query.on('end', () => 
+     { console.log(rows);
+       console.log('query done')
+       res.send(rows);
+       res.status(200).end();
+    });
+    query.on('error', err => {
+         console.error(err.stack)
+    });
+});
+
+
+//그래프 출력 위한 alltime API
+app.get('/api/alltime/:currencyCode', function(req, res, next) {
+    console.log("SELECT * FROM " +req.params.currencyCode)
+    
+    const query = new Query("SELECT * FROM " +req.params.currencyCode);
+    client.query(query)
+    
+    var rows = [];
+    query.on("row",row=>{
+          rows.push(row);
+     });
+     
+    query.on('end', () => 
+     { console.log(rows);
+       console.log('query done')
+       res.send(rows);
+       res.status(200).end();
+    });
+
+    query.on('error', err => {
+         console.error(err.stack)
+    });
+});
+
+
+app.listen(PORT, HOST,() => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
